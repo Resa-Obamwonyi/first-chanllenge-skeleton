0259f4cd3bfc7879d53eea3b255d92e0a74170ee
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-06-17 15:21:36 -0700

    second commit

--- challenge.py
+++ challenge.py
@@ -1,14 +1,12 @@
-"""Coding Challenge #1
+"""Coding Challenge Skeleton #1
+This counter function purpose is to count how many english letters does your name contain.
+After writing your tests, develop the counter function as needed to pass all your tests.
+
 """
 
 
-def counter(name: str) -> int:
-    name = name.replace(' ', '')
-    if len(name) == 0:
-        raise Exception("Please input your name.")
-    if name.isalpha():
-        return len(name)
-    else:
-        raise Exception("The string must contain english letters only.")
+def counter(name):
+    # Todo: implement me
+    return len(name)
 
 
--- tests/test_chanllege.py
+++ tests/test_chanllege.py
@@ -5,34 +5,31 @@ from challenge import counter
 class EasyTestCase(unittest.TestCase):
     def test_easy_input(self):
         # Todo: make sure that your program returns 2 given the string 'Mo'
-        self.assertEqual(counter("Mo"), 2)
+        pass
 
     def test_easy_input_two(self):
         # Todo: make sure that your program returns 8 given the string 'Mohammad'
-        self.assertEqual(counter("Mohammad"), 8)
+        pass
 
 
 class MediumTestCase(unittest.TestCase):
     def test_medium_input(self):
-        with self.assertRaises(Exception):
-            # Todo: make sure that the program raises an exception whenever there is any non-english charts.
-            self.assertEqual(counter("Mo?."), 4)
+        # Todo: make sure that the program raises an exception whenever there is any non-english charts. Ex. !@#$%^.
+        pass
 
     def test_medium_input_two(self):
         # Todo: make sure that your program does not count paces. It should only count english alpha.
-        self.assertEqual(counter("Mohammad Mahjoub"), 15)
+        pass
 
 
 class HardTestCase(unittest.TestCase):
     def test_hard_input(self):
-        with self.assertRaises(Exception):
-            # Todo: make sure that your program does not accept an empty string.
-            self.assertEqual(counter(""), 0)
+        # Todo: make sure that the program raises an exception whenever an empty string is given.
+        pass
 
     def test_hard_input_two(self):
-        with self.assertRaises(Exception):
-            # Todo: make sure that your program does not accept a None input.
-            self.assertEqual(counter(None), 0)
+        # Todo: make sure that your program does not accept a None input.
+        pass
 
 
 if __name__ == '__main__':
